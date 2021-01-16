530840866bfccdbfa69f3c4d610b7acc372fdb97
Metaxal <laurent.orseau@gmail.com>
2021-01-16 11:31:21 +0000

    fix shadow script and add more information

--- shadow-script.rkt
+++ shadow-script.rkt
@@ -14,13 +14,19 @@
 (require quickscript
          (prefix-in @shadow-prefix (file @(~s (path->string f)))))
 
+;;; This is a 'shadow' script.
+;;; The script functions below call the functions of the original script,
+;;; but you can edit their properties to match your needs.
+;;; See the documentation for `define-script` for more information about the
+;;; different properties.
+
  })
 
 ;; todo: change properties only if not default?
-(define (shadow-script-proc fun-sym props)
+(define (shadow-script-proc props)
   (define (dstr sym)
     (~s (dict-ref props sym)))
-  (define fun-str (symbol->string fun-sym))
+  (define fun-str (dstr 'name))
 @string-append{
 (define-script @fun-str
   #:label @(dstr 'label)
@@ -33,8 +39,7 @@
         "")
   @|shadow-prefix|@fun-str)
   
-}
-)
+})
 
 (define (make-shadow-script f)
   (parameterize ([current-namespace (make-base-empty-namespace)])
@@ -44,14 +49,27 @@
      (make-header f)
      "\n"
      (string-join
-      (for/list ([(fun props) (in-dict props-dict)])
-        (shadow-script-proc fun props))
+      (for/list ([props (in-list props-dict)])
+        (shadow-script-proc props))
       "\n"))))
 
 
+(module+ test
+  (require syntax/modresolve
+           racket/path
+           rackunit)
+  ; don't bother if the module does not exist.
+  (with-handlers ([exn:fail:filesystem:missing-module? void])
+    (define qs-path (resolve-module-path 'quickscript-extra))
+    (define f (build-path (path-only qs-path)
+                          "scripts" "bookmarks.rkt"))
+    (check-not-exn (λ () (make-shadow-script f)))))
+
+;;; To see an actual output
 (module+ main
   (require syntax/modresolve
-           racket/path)
+           racket/path
+           rackunit)
   ; don't bother if the module does not exist.
   (with-handlers ([exn:fail:filesystem:missing-module? void])
     (define qs-path (resolve-module-path 'quickscript-extra))
